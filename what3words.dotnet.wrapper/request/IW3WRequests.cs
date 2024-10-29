4bd6188001b4570ee1ab1e749eec467d3cc9c29e
Frederick Lee <frederickrichard.lee@gmail.com>
2024-10-29 14:19:39 +0000

    fix: moved Request class to a namespace

--- what3words.dotnet.wrapper/request/IW3WRequests.cs
+++ what3words.dotnet.wrapper/request/IW3WRequests.cs
@@ -6,70 +6,74 @@ using what3words.dotnet.wrapper.request;
 using what3words.dotnet.wrapper.response;
 using static what3words.dotnet.wrapper.request.ConvertTo3WARequest;
 
+
+namespace what3words.dotnet.wrapper.request
+{
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-public interface IW3WRequests
-{
-
-    [Get("/convert-to-3wa")]
-    Task<Address> ConvertTo3WA(ConvertTo3WAOptions options);
-
-    [Get("/convert-to-coordinates?words={words}")]
-    Task<Address> ConvertToCoordinates(string words);
-
-    [Get("/available-languages")]
-    Task<AvailableLanguages> AvailableLanguages();
-
-    [Get("/grid-section?bounding-box={boundingBox}")]
-    Task<GridSection> GridSection(string boundingBox);
-
-    [Get("/autosuggest")]
-    Task<AutoSuggest> AutoSuggest(string input, AutosuggestOptions options);
-
-    [Get("/autosuggest-with-coordinates")]
-    Task<AutoSuggestWithCoordinates> AutoSuggestWithCoordinates(string input, AutosuggestOptions options);
-
-    [Get("/autosuggest-selection?raw-input={rawInput}&selection={selection}&rank={rank}&source-api={sourceApi}")]
-    Task AutoSuggestSelection(string rawInput, string selection, int? rank, string sourceApi, AutosuggestOptions options);
-}
-
-public abstract class Request<T>
-{
-    protected abstract Task<T> ApiRequest { get; }
-
-    /**
-       * <summary> Request API call asynchronously </summary>
-       */
-    public async Task<APIResponse<T>> RequestAsync()
+    public interface IW3WRequests
     {
-        try
+
+        [Get("/convert-to-3wa")]
+        Task<Address> ConvertTo3WA(ConvertTo3WAOptions options);
+
+        [Get("/convert-to-coordinates?words={words}")]
+        Task<Address> ConvertToCoordinates(string words);
+
+        [Get("/available-languages")]
+        Task<AvailableLanguages> AvailableLanguages();
+
+        [Get("/grid-section?bounding-box={boundingBox}")]
+        Task<GridSection> GridSection(string boundingBox);
+
+        [Get("/autosuggest")]
+        Task<AutoSuggest> AutoSuggest(string input, AutosuggestOptions options);
+
+        [Get("/autosuggest-with-coordinates")]
+        Task<AutoSuggestWithCoordinates> AutoSuggestWithCoordinates(string input, AutosuggestOptions options);
+
+        [Get("/autosuggest-selection?raw-input={rawInput}&selection={selection}&rank={rank}&source-api={sourceApi}")]
+        Task AutoSuggestSelection(string rawInput, string selection, int? rank, string sourceApi, AutosuggestOptions options);
+    }
+
+    public abstract class Request<T>
+    {
+        protected abstract Task<T> ApiRequest { get; }
+
+        /**
+           * <summary> Request API call asynchronously </summary>
+           */
+        public async Task<APIResponse<T>> RequestAsync()
         {
-            return new APIResponse<T>(await ApiRequest);
-        }
-        catch (Refit.ApiException e)
-        {
-            var apiException = await e.GetContentAsAsync<what3words.dotnet.wrapper.response.ApiException>();
-            if (apiException != null)
+            try
             {
-                return new APIResponse<T>(apiException.Error);
+                return new APIResponse<T>(await ApiRequest);
             }
-            else
+            catch (Refit.ApiException e)
+            {
+                var apiException = await e.GetContentAsAsync<what3words.dotnet.wrapper.response.ApiException>();
+                if (apiException != null)
+                {
+                    return new APIResponse<T>(apiException.Error);
+                }
+                else
+                {
+                    var error = new APIError
+                    {
+                        Code = What3WordsError.UnknownError.ToString(),
+                        Message = e.Message
+                    };
+                    return new APIResponse<T>(error);
+                }
+            }
+            catch (Exception e)
             {
                 var error = new APIError
                 {
-                    Code = What3WordsError.UnknownError.ToString(),
+                    Code = What3WordsError.NetworkError.ToString(),
                     Message = e.Message
                 };
                 return new APIResponse<T>(error);
             }
-        }
-        catch (Exception e)
-        {
-            var error = new APIError
-            {
-                Code = What3WordsError.NetworkError.ToString(),
-                Message = e.Message
-            };
-            return new APIResponse<T>(error);
         }
     }
 }
